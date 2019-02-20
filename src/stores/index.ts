6cae17360011266c59f8f5e77fd3105357950c75
sonhoai <sonhoai272@gmail.com>
2019-02-20 15:53:21 +0700

    change style of define name

--- src/stores/index.ts
+++ src/stores/index.ts
@@ -2,8 +2,8 @@ import { createStore, applyMiddleware } from 'redux';
 import createSagaMiddleware from 'redux-saga'
 import loggerMiddleware from '@app/configs/LoggerMiddleware';
 
-import rootReducer from './reducers';
-import rootSaga from './rootSaga';
+import rootReducer from './Reducers';
+import rootSaga from './RootSaga';
 
 const sagaMiddleware = createSagaMiddleware()
 
